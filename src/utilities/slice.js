122f6995cb59680eb36b98d950ec5cf561c91750
Sebastián González <sebastian.gonzalez@glovoapp.com>
2024-05-19 09:40:39 +0200

    fix: slice units

--- src/utilities/slice.js
+++ src/utilities/slice.js
@@ -1,3 +1,68 @@
+const values = {
+  0: '0',
+  1: '1',
+  2: '2',
+  4: '4',
+  6: '6',
+  8: '8',
+  10: '10',
+  12: '12',
+  14: '14',
+  16: '16',
+  20: '20',
+  24: '24',
+  28: '28',
+  32: '32',
+  36: '36',
+  40: '40',
+  44: '44',
+  48: '48',
+  56: '56',
+  64: '64',
+  80: '80',
+  96: '96',
+  112: '112',
+  128: '128',
+  144: '144',
+  160: '160',
+  176: '176',
+  192: '192',
+  208: '208',
+  224: '224',
+  240: '240',
+  256: '256',
+  288: '288',
+  320: '320',
+  384: '384',
+  '1/2': '50%',
+  '1/3': '33.333333%',
+  '2/3': '66.666667%',
+  '1/4': '25%',
+  '2/4': '50%',
+  '3/4': '75%',
+  '1/5': '20%',
+  '2/5': '40%',
+  '3/5': '60%',
+  '4/5': '80%',
+  '1/6': '16.666667%',
+  '2/6': '33.333333%',
+  '3/6': '50%',
+  '4/6': '66.666667%',
+  '5/6': '83.333333%',
+  '1/12': '8.333333%',
+  '2/12': '16.666667%',
+  '3/12': '25%',
+  '4/12': '33.333333%',
+  '5/12': '41.666667%',
+  '6/12': '50%',
+  '7/12': '58.333333%',
+  '8/12': '66.666667%',
+  '9/12': '75%',
+  '10/12': '83.333333%',
+  '11/12': '91.666667%',
+  full: '100%',
+}
+
 const sliceUtilities = ({ addUtilities, matchUtilities, theme }) => {
   matchUtilities(
     {
@@ -9,7 +74,7 @@ const sliceUtilities = ({ addUtilities, matchUtilities, theme }) => {
       }),
     },
     {
-      values: theme('width'),
+      values,
     },
   )
 
@@ -21,7 +86,7 @@ const sliceUtilities = ({ addUtilities, matchUtilities, theme }) => {
       }),
     },
     {
-      values: theme('width'),
+      values,
     },
   )
 
@@ -33,7 +98,7 @@ const sliceUtilities = ({ addUtilities, matchUtilities, theme }) => {
       }),
     },
     {
-      values: theme('height'),
+      values,
     },
   )
 
@@ -44,7 +109,7 @@ const sliceUtilities = ({ addUtilities, matchUtilities, theme }) => {
       }),
     },
     {
-      values: theme('height'),
+      values,
     },
   )
 
@@ -55,7 +120,7 @@ const sliceUtilities = ({ addUtilities, matchUtilities, theme }) => {
       }),
     },
     {
-      values: theme('width'),
+      values,
     },
   )
 
@@ -66,7 +131,7 @@ const sliceUtilities = ({ addUtilities, matchUtilities, theme }) => {
       }),
     },
     {
-      values: theme('height'),
+      values,
     },
   )
 
@@ -77,7 +142,7 @@ const sliceUtilities = ({ addUtilities, matchUtilities, theme }) => {
       }),
     },
     {
-      values: theme('width'),
+      values,
     },
   )
 
